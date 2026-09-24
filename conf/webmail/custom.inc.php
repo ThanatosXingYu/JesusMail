c36038f090424921c7ae42795f0071639d27ee27
Thanatos <574401159@qq.com>
2026-09-25 01:31:20 +0800

    fix(webmail): 默认使用简体中文登录界面

--- conf/webmail/custom.inc.php
+++ conf/webmail/custom.inc.php
@@ -1,6 +1,7 @@
 <?php
     # Product name
     $config['product_name'] = 'JesusMail';
+    $config['language'] = 'zh_CN'; // Default webmail locale; users can change it in preferences.
     $config['display_product_info'] = false;
 
     $config['mime_types'] = '/var/roundcube/config/mime.types';
